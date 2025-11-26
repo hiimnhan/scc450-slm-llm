1a9c108e2843486d96ead221000fc6ac93c82e90
xbradmx <bradders0903@gmail.com>
2025-11-26 20:24:15 +0000

    pushing next part of tokenizer to git

--- tokenizer_doc.py
+++ tokenizer_doc.py
@@ -101,23 +101,33 @@ print(f'\n new subfolder paths are: {output_subfolder_paths}') # path checking
 
 
 #==================================================
-# accessing json file in subfolders + tokenization
+# gathering json file paths
 #==================================================
 
 def json_paths(subfolder_paths, output_subfolder_pathing):
 
+    # gives the names of the json files
     subfolder_json_file_names = [os.listdir(subfolder_path) for subfolder_path in subfolder_paths]
 
-    print(len(subfolder_json_file_names))
-    print(len(output_subfolder_pathing))
+    # checking len of lists
+    # print(len(subfolder_json_file_names))
+    # print(len(output_subfolder_pathing))
 
+    new_subfolder_json_pathing = [] # give absolute path so order irrelevant at this point
+    old_subfolder_json_pathing = []
 
-
-    for subfolder, output_path in zip(subfolder_json_file_names, output_subfolder_pathing):
+    for subfolder, output_path, old_path in zip(subfolder_json_file_names, output_subfolder_pathing, subfolder_paths):
         # print(f'\nthis is a subfolder:\n {subfolder}\n')
         for file in subfolder:
             print(f'\nthis is a file in a subfolder:\n {file}')
-            print(f' this file is allocated to path: {output_path}')
+            print(f' this file is allocated to new path: {output_path}')
+            print(f' this file is allocated to old path: {old_path}')
+
+            new_file_json_pathing = os.path.join(output_path, file)
+            old_file_json_pathing = os.path.join(old_path, file)
+
+            new_subfolder_json_pathing.append(new_file_json_pathing)
+            old_subfolder_json_pathing.append(old_file_json_pathing)
 
         print(f'\n#=================================#')
         print(f'#=================================#')
@@ -129,20 +139,60 @@ def json_paths(subfolder_paths, output_subfolder_pathing):
 
         # subfolder_json_pathing = [os.path.join(output_path, files) for files in subfolders]
 
-    return subfolder_json_file_names #, subfolder_json_pathing
+    return subfolder_json_file_names, new_subfolder_json_pathing, old_subfolder_json_pathing
 
-json_file_names = json_paths(subfolder_paths_list, output_subfolder_paths)
+json_file_names, output_json_file_paths, old_json_file_paths = json_paths(subfolder_paths_list, output_subfolder_paths)
 
-print(f'\n new json names are: {json_file_names}') # name checking
-# print(f'\n new json paths are: {json_file_paths}') # path checking  , json_file_paths
-
-
-def tokenize_json():
-    pass
+print(f'\n new json names are:\n{json_file_names}') # name checking
+print(f'\n old json paths are:\n{old_json_file_paths}') # path checking
+print(f'\n new json paths are:\n{output_json_file_paths}') # path checking
 
 
 
 
+#========================================================
+# tokenizing json files and sending them to new path file
+#========================================================
+
+
+
+
+def tokenize_json(old_paths, new_paths):
+    '''
+    access json one by one
+
+    tokenize the json file
+    -
+
+    send the file to new path in tokenised_trafford
+    '''
+
+    print(f'\n\nJSON data starts from here:')
+    print(f'JSON data starts from here:')
+    print(f'JSON data starts from here:')
+    print(f'JSON data starts from here:')
+    print(f'JSON data starts from here:\n\n')
+
+    for json_path in old_paths:
+        with open(json_path, 'r', encoding='utf-8') as f:
+            json_data = json.load(f)
+            json_data = pd.DataFrame(json_data)
+            print(f'\n{json_data.columns}\n')
+            print(f'\n{json_data.rows}\n')
+
+
+            # json_data = pd.DataFrame(json_data)
+            # print(json_data['type'].value_counts())
+            # # print(json_data.head(20))
+
+
+
+
+
+            break
+
+
+tokenize_json(old_json_file_paths, output_json_file_paths)
 
 
 def iterate_through_jsons_in_subfiles():
